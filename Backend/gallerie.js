9ae0a24d41a36d56457d513500a78b59ca46aa83
Rean18 <Rean18@github.com>
2023-11-24 16:42:51 +0100

    Ajout ddu fichier gallerie en javascript

---
+++ Backend/gallerie.js
@@ -0,0 +1,33 @@
+const urlAPI = "http://localhost:5678/api/works";
+
+async function recupererTravaux () {
+    let requete = fetch(urlAPI);
+
+    if(!requete.ok) {
+        console.log("Un problème est survenu");
+
+    } else {
+        let travaux = await requete.json();
+
+        for(let i = 0; i < travaux.length; i++) {
+
+            let projet = travaux[i];
+
+            // Création des éléments et du contenu de la gallerie
+            let recupGallery = document.querySelector(".gallery");
+            let figure = document.createElement("figure");
+            let imageProjet = document.createElement("img");
+            imageProjet.src = travaux.imageUrl;
+            let titreProjet = document.createElement("figcaption");
+            titreProjet.innerText = travaux.title;
+
+            // Association des éléments parents/enfants
+            recupGallery.appendChild(figure);
+            figure.appendChild(imageProjet);
+            figure.appendChild(titreProjet);
+        
+        }
+    }
+
+}
+recupererTravaux();
