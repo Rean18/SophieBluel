01b13150e414c9a3df8775bd2a47c69afa6c2e1e
Rean18 <Rean18@github.com>
2023-11-29 16:27:49 +0100

    Déplacement du fichier gallerie.js et mise en place des filtres

---
+++ Backend/filtre.js
@@ -0,0 +1,29 @@
+
+// async function recupererObjets() {
+//     let requete = await fetch(urlAPI);
+
+//     if(!requete.ok) {
+//         console.log("Un problème est survenu");
+
+//     } else {
+//         let travaux = await requete.json();
+//         console.log(travaux);
+
+//         const boutonFiltreObjet = document.querySelector(".btn-objet");
+//         boutonFiltreObjet.addEventListener("click", function() {
+//             console.log("Bouton cliqué");
+//         const tousObjets = travaux.filter(function(travail){
+//         return travail.category.name === "Objets"
+//     })
+//     console.log(tousObjets);
+    
+// })
+        
+//         }
+//     }
+    
+// recupererObjets()
+
+
+// // Filtre de la gallerie photo
+
